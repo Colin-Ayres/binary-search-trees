bc9662f421a500434d0d895a14195f3022c6c749
Colin Ayres <CAyres7@gmail.com>
2022-04-11 00:01:26 -0400

    Initial commit

---
+++ lib/main.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative 'node'
+require_relative 'tree'
+
+my_tree = BinarySearchTree.new
---
+++ lib/tree.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative 'node'
+
+# This class recreates the functionality of a Binary Search Tree
+class LinkedList
+  attr_reader :head, :tail, :size
+
+  def initialize
+    #@head = nil
+    #@tail = nil
+    #@size = 0
+  end
+
+end
